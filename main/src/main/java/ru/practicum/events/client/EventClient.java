3b52c7adf964d149b1e548cdc5f444dd4a09559d
Sasha1239 <ham1239@gmail.com>
2022-11-29 20:11:54 +0300

    refactor: прохождение checkstyle

--- main/src/main/java/ru/practicum/events/client/EventClient.java
+++ main/src/main/java/ru/practicum/events/client/EventClient.java
@@ -15,11 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Component
 public class EventClient {
